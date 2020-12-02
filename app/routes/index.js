1cb2eff076584e5f26745212ebc4e53a8e8d8c1f
Ljuniorcode <lucianojuniorarl@gmail.com>
2020-12-02 01:24:06 -0300

    02 - recebendo arquivos no nodejs com formidable

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let formidable = require('formidable')
+
+let router = express.Router();
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/upload', (req, res) => {
+
+  let form = new formidable.IncomingForm({ //recuperando o formulario
+    uploadDir: './upload',
+    keepExtensions: true,
+  })
+
+  //tratando as requisições
+  form.parse(req, (err, fields, files) => {
+    res.json({
+      files
+    })
+  })
+
+
+  res.json(req.body)
+})
 
 module.exports = router;
